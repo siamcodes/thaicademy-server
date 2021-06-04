2fb09665ea1e1a10add8f339fe36a42f100c3e75
siamcodes <siamcodes@gmail.com>
2021-06-04 14:11:16 +0700

    first commit

---
+++ middlewares/index.js
@@ -0,0 +1,89 @@
+import User from "../models/user";
+import Course from "../models/course";
+import expressJwt from "express-jwt";
+
+export const requireSignin = expressJwt({
+  getToken: (req, res) => req.cookies.token,
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"],
+});
+
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).exec();
+
+    if (!user.role.includes("Admin")) {
+      res.sendStatus(403);
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const isInstructor = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).exec();
+    if (!user.role.includes("Instructor")) {
+      return res.sendStatus(403);
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const isAuthor = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).exec();
+
+    if (!user.role.includes("Author")) {
+      res.sendStatus(403);
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+/**
+export const canEditDeleteCourse = async (req, res, next) => {
+  try {
+    const course = await Course.findOne({ slug: req.params.slug }).exec();
+    if (course.instructor.toString() !== req.user._id.toString()) {
+      res.sendStatus(403);
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+ */
+
+export const isEnrolled = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).exec();
+    const course = await Course.findOne({ slug: req.params.slug }).exec();
+
+    // check if hotel id is found in userOrders array
+    let ids = [];
+    for (let i = 0; i < user.courses.length; i++) {
+      ids.push(user.courses[i].toString());
+    }
+
+    if (!ids.includes(course._id.toString())) {
+      res.sendStatus(403);
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
